8d99dcb5053ce8db1028b49634ee47dc0b5e7b0b
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-13 22:36:44 -0300

    adiciona hook useGetGastos para carregamento de gastos

---
+++ resources/js/hooks/gastos/useGetGastos.tsx
@@ -0,0 +1,29 @@
+import { http, isApiError } from '@/lib/http';
+import { ApiGasto } from '@/types/ApiGasto';
+import { useState } from 'react';
+
+export function useGetGastos() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [gastos, setGastos] = useState<ApiGasto[]>([]);
+
+    const loadGastos = async () => {
+        setIsLoading(true);
+        setErrorMessage(null);
+
+        try {
+            const response = await http.get<{ data: ApiGasto[] }>('/gastos');
+            setGastos(response.data.data);
+        } catch (error) {
+            if (isApiError(error)) {
+                setErrorMessage(error.response?.data?.message ?? 'Erro ao carregar gastos.');
+            } else {
+                setErrorMessage('Erro ao carregar gastos.');
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { loadGastos, isLoading, errorMessage, gastos };
+}
