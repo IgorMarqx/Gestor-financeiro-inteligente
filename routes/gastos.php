473a483a7b5e11fde2443632a13d4196bd6e4178
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-13 22:54:52 -0300

    adiciona rotas para atualização e remoção de gastos na API

--- routes/gastos.php
+++ routes/gastos.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('gastos')->group(function () {
     Route::get('/', [GastosController::class, 'index'])->name('api.gastos.index');
     Route::post('/', [GastosController::class, 'store'])->name('api.gastos.store');
+    Route::put('/{gasto}', [GastosController::class, 'update'])->name('api.gastos.update');
+    Route::delete('/{gasto}', [GastosController::class, 'destroy'])->name('api.gastos.destroy');
 });
