c6f8303a9b050fe09a688668553ec653bb5e8b39
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-14 17:15:00 -0300

    adiciona rota para validação de gastos

--- routes/gastos.php
+++ routes/gastos.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('gastos')->group(function () {
     Route::get('/', [GastosController::class, 'index'])->name('api.gastos.index');
     Route::post('/', [GastosController::class, 'store'])->name('api.gastos.store');
+    Route::post('/validar', [GastosController::class, 'validar'])->name('api.gastos.validar');
     Route::put('/{gasto}', [GastosController::class, 'update'])->name('api.gastos.update');
     Route::delete('/{gasto}', [GastosController::class, 'destroy'])->name('api.gastos.destroy');
 });
