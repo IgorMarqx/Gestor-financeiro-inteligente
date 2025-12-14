0408b4e8710ac43fc8e90472bd47244f2758843a
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-14 17:04:35 -0300

    adiciona modelo Conta para gerenciamento de contas financeiras

---
+++ app/Models/Conta.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conta extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contas';
+
+    protected $fillable = [
+        'usuario_id',
+        'nome',
+        'tipo',
+        'saldo_inicial',
+    ];
+
+    protected $casts = [
+        'saldo_inicial' => 'decimal:2',
+    ];
+}
+
