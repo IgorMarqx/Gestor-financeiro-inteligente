68a0a1f019952d42af1b1cfe398eea666be73056
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-13 22:37:32 -0300

    adiciona rotas para gastos e categorias no dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('gastos', function () {
+        return Inertia::render('gastos/index');
+    })->name('gastos.index');
+
+    Route::get('categorias', function () {
+        return Inertia::render('categorias-gastos/index');
+    })->name('categorias.index');
 });
 
 require __DIR__.'/settings.php';
