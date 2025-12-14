5a65f465b428f9c9fd284f9d626efb8f170b0770
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-14 18:41:42 -0300

    refatora uso de efeitos no hook useParcelasHistory para garantir atualização correta dos filtros

--- resources/js/hooks/gastos-parcelamentos/useParcelasHistory.tsx
+++ resources/js/hooks/gastos-parcelamentos/useParcelasHistory.tsx
@@ -58,8 +58,17 @@ export function useParcelasHistory(initial: ParcelasFilters) {
 
     useEffect(() => {
         void load();
-    }, [params]);
+    }, []);
+
+    useEffect(() => {
+        setFilters((f) => ({
+            ...f,
+            inicio: initial.inicio,
+            fim: initial.fim,
+            status: initial.status,
+            page: 1,
+        }));
+    }, [initial.inicio, initial.fim, initial.status]);
 
     return { filters, setFilters, data, isLoading, errorMessage, reload: load };
 }
-
