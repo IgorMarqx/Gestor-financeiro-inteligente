53b90fb77d6b8d198bb416704aea5145dd94e331
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-14 17:04:30 -0300

    adiciona modelo Ativo para gerenciamento de ativos financeiros

---
+++ app/Models/Ativo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ativo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ativos';
+
+    protected $fillable = [
+        'usuario_id',
+        'nome',
+        'tipo',
+        'ticker',
+    ];
+}
+
