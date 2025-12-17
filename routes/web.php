9f4911c8666ab5448b79f6ec9195761b49291d7c
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-17 16:47:11 -0300

    remove rota 'chat' e controlador ChatController do arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
-use App\Http\Controllers\ChatController;
 use App\Http\Controllers\Web\WebController;
 
 Route::get('/', function () {
@@ -16,7 +15,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [WebController::class, 'dashboard'])->name('dashboard');
     Route::get('gastos', [WebController::class, 'gastosIndex'])->name('gastos.index');
     Route::get('categorias', [WebController::class, 'categoriasIndex'])->name('categorias.index');
-    Route::get('chat', [WebController::class, 'chatIndex'])->name('chat.index');
 });
 
 require __DIR__ . '/settings.php';
