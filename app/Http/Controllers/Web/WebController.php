990ee8754db727251e236c432e64639c14e6c16f
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-16 22:04:02 -0300

    adiciona controlador WebController com métodos para renderizar páginas do dashboard, gastos, categorias e chat

---
+++ app/Http/Controllers/Web/WebController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class WebController extends Controller
+{
+    public function dashboard()
+    {
+        return Inertia::render('dashboard');
+    }
+
+    public function gastosIndex()
+    {
+        return Inertia::render('gastos/index');
+    }
+
+    public function categoriasIndex()
+    {
+        return Inertia::render('categorias-gastos/index');
+    }
+
+    public function chatIndex()
+    {
+        return Inertia::render('chat/index');
+    }
+}
