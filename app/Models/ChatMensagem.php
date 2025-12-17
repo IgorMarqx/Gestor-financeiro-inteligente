c23cbbeef73da11caa3d692361080b5e95bec8ad
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-16 22:04:07 -0300

    adiciona modelo ChatMensagem com atributos e relacionamentos

---
+++ app/Models/ChatMensagem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChatMensagem extends Model
+{
+    protected $table = 'chat_mensagens';
+
+    protected $fillable = [
+        'chat_id',
+        'role',
+        'conteudo',
+        'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class, 'chat_id');
+    }
+}
+
