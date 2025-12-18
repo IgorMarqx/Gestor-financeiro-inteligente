d8ba0c16397b1f8082f986a411ae9b48f2e23268
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-17 22:41:04 -0300

    adiciona rotas para resumo, gastos, atualização e exclusão de categorias

--- routes/categorias-gastos.php
+++ routes/categorias-gastos.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('categorias-gastos')->group(function () {
     Route::get('/', [CategoriasGastosController::class, 'index'])->name('api.categorias_gastos.index');
+    Route::get('/resumo', [CategoriasGastosController::class, 'resumo'])->name('api.categorias_gastos.resumo');
+    Route::get('/{id}/gastos', [CategoriasGastosController::class, 'gastos'])->name('api.categorias_gastos.gastos');
     Route::post('/', [CategoriasGastosController::class, 'store'])->name('api.categorias_gastos.store');
+    Route::put('/{id}', [CategoriasGastosController::class, 'update'])->name('api.categorias_gastos.update');
+    Route::delete('/{id}', [CategoriasGastosController::class, 'destroy'])->name('api.categorias_gastos.destroy');
 });
-
