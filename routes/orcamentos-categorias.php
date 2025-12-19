18d80eae305fd7fa2a027d175f6cf5fb62c27404
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-18 21:06:57 -0300

    adiciona rotas para upsert e exclusão em massa de categorias

--- routes/orcamentos-categorias.php
+++ routes/orcamentos-categorias.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('orcamentos-categorias')->group(function () {
     Route::get('/', [OrcamentosCategoriasController::class, 'index'])->name('api.orcamentos_categorias.index');
     Route::post('/', [OrcamentosCategoriasController::class, 'store'])->name('api.orcamentos_categorias.store');
+    Route::post('/upsert', [OrcamentosCategoriasController::class, 'upsert'])->name('api.orcamentos_categorias.upsert');
+    Route::post('/batch-upsert', [OrcamentosCategoriasController::class, 'batchUpsert'])->name('api.orcamentos_categorias.batch_upsert');
     Route::put('/{id}', [OrcamentosCategoriasController::class, 'update'])->name('api.orcamentos_categorias.update');
     Route::delete('/{id}', [OrcamentosCategoriasController::class, 'destroy'])->name('api.orcamentos_categorias.destroy');
+    Route::delete('/', [OrcamentosCategoriasController::class, 'destroyByCategoriaMes'])->name('api.orcamentos_categorias.destroy_by_categoria_mes');
     Route::get('/resumo', [OrcamentosCategoriasController::class, 'resumo'])->name('api.orcamentos_categorias.resumo');
 });
-
