0fb580953a003d1f20a428472374900ff30dbc3a
Igor Marques <igormarquesdeazevedo11@gmail.com>
2025-12-19 12:28:08 -0300

    adiciona tipos para Pusher e Echo no ambiente Vite

--- resources/js/types/vite-env.d.ts
+++ resources/js/types/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+import Pusher from 'pusher-js';
+import Echo from 'laravel-echo';
+
+declare global {
+    interface Window {
+        Pusher: typeof Pusher;
+        Echo: Echo;
+    }
+}
+
+export {};
