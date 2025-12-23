d169b384c069fe3644bf03d6c9ab2a8356baa746
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-12-23 20:53:33 -0300

    adiciona método createUserByFamily no repositório UserRepository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repositories;
 
 use App\Models\User;
-
 class UserRepository
 {
     public function findByEmail(string $email): ?User
@@ -16,5 +15,16 @@ class UserRepository
     public function findById(int $id): ?User
     {
         return User::query()->find($id);
+    }
+
+    public function createUserByFamily(array $payload)
+    {
+        return User::query()->create([
+            'name' => $payload['nome'] ?? '',
+            'email' => $payload['email'],
+            'cpf' => $payload['cpf'] ?? null,
+            'telefone' => $payload['telefone'] ?? null,
+            'password' => $payload['senha'],
+        ]);
     }
 }
