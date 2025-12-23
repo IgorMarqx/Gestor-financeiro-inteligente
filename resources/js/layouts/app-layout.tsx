c1b4856156aaec9fafc9ae782aa688c0b9fa4272
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2025-12-22 21:05:28 -0300

    adiciona lógica para exibir o ChatWidget apenas em páginas que não são de chat

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -2,6 +2,8 @@ import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { type ReactNode } from 'react';
 import { GlobalModals } from '@/components/modals/GlobalModals';
+import { ChatWidget } from '@/components/chat/chat-widget';
+import { usePage } from '@inertiajs/react';
 
 interface AppLayoutProps {
     children: ReactNode;
@@ -9,12 +11,15 @@ interface AppLayoutProps {
 }
 
 export default function AppLayout({ children, breadcrumbs, ...props }: AppLayoutProps) {
+    const page = usePage();
 
     return (
         <>
             <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
                 {children}
             </AppLayoutTemplate>
+
+            {page.url.includes('/chat') ? null : <ChatWidget />}
             <GlobalModals />
         </>
     );
